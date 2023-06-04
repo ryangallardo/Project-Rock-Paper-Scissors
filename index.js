cdca6630fdaff631fc71b1ba550279213713a98d
Ryan Gallardo <ryangallardo1998@gmail.com>
2023-06-04 17:33:25 +0800

    Add function for play round

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1
     switch(randomNumber) {
-        case 1: console.log("Rock")
+        case 1: return "Rock"
         break;
-        case 2: console.log("Paper")
+        case 2: return "Paper"
         break;
-        case 3: console.log("Scissor")
+        case 3: return "Scissor"
         break;
     }
 }
-getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    let player = playerSelection.toLowerCase();
+    let comp = computerSelection.toLowerCase();
+
+    if (player === "rock") {
+        if (comp === "paper") {
+            return "You Lose! Paper beats Rock";
+        }else if (comp === "scissor") {
+            return "You Win! Rock beats Scissor";
+        }else {
+            return "It's a tie!";
+        }
+    }else if (player === "paper") {
+        if (comp === "rock") {
+            return "You Win! Paper beats Rock";
+        }else if (comp === "scissor") {
+            return "You Lose! Scissor beats Paper";
+        }else {
+            return "It's a tie!";
+        }
+    }else if (player === "Scissor") {
+        if (comp === "rock") {
+            return "You Lose! Rock beats Scissor";
+        }else if (comp === "paper") {
+            return "You Win! Scissor beats Paper";
+        }else {
+            return "It's a tie!";
+        }
+    }else {
+        return "Please enter valid input!"
+    }
+
+}
+   
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
